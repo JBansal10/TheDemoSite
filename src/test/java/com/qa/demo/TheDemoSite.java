31473d9505e2a296585e3d8a8485bbced4137729
Jbansal10 <jbansal@academytrainee.com>
2021-01-25 14:37:14 +0000

    first commit

---
+++ src/test/java/com/qa/demo/TheDemoSite.java
@@ -0,0 +1,55 @@
+package com.qa.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TheDemoSite {
+	
+	private static WebDriver driver;
+	private static WebElement targ;
+
+	@BeforeAll
+	public static void init() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chrome/chromedriver.exe");
+		driver = new ChromeDriver(chromeCfg());
+
+	}
+
+	
+	@Test
+	public void testDemo() {
+		
+	}
+	
+	@AfterAll
+	public static void cleanUp() {
+		driver.quit();
+		System.out.println("The Selenium Driver has been cleaned up!");
+	}
+
+	public static ChromeOptions chromeCfg() {
+
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		ChromeOptions cOptions = new ChromeOptions();
+
+		// Settings
+		prefs.put("profile.default_content_setting_values.cookies", 2);
+		prefs.put("network.cookie.cookieBehavior", 2);
+		prefs.put("profile.block_third_party_cookies", true);
+
+		// Create ChromeOptions to disable Cookies pop-up
+		cOptions.setExperimentalOption("prefs", prefs);
+
+		return cOptions;
+
+	}
+
+}
